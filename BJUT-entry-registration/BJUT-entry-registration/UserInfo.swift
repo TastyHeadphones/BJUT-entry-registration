82d59a5277515f1dbf549edae2f49c6a8002da0e
TastyHeadphones <76083688+TastyHeadphones@users.noreply.github.com>
2022-04-08 21:43:40 +0800

    初步设计使用UI

---
+++ BJUT-entry-registration/BJUT-entry-registration/UserInfo.swift
@@ -0,0 +1,11 @@
+//
+//  UserInfo.swift
+//  BJUT-entry-registration
+//
+//  Created by Magic Keegan on 4/8/22.
+//
+
+struct UserInfo{
+    static let UUKey = "9fbf9b738f87ffa3b5b44481846e4385"
+    static let cookie = "eai-sess=55tsnk35s736c9u9imelhq2tn3"
+}
---
+++ BJUT-entry-registration/BJUT-entry-registration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BJUT-entry-registration
+//
+//  Created by Magic Keegan on 4/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
